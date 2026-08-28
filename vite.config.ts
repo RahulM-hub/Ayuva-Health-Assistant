85d18333bd326bd53190e171c8b8db440ea3ea2f
RahulM-hub <rahulms0930@gmail.com>
2026-08-28 18:03:39 +0530

    Fix Github pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Ayuva-Health-Assistant/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
